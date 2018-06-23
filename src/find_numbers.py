bb27bacdc5f4bab4687577923217ff4fc30a588b
Stan Chan <stanchanpsu@outlook.com>
2018-06-23 10:32:44 -0400

    Clean up code; get rid of magic numbers

--- src/find_numbers.py
+++ src/find_numbers.py
@@ -14,42 +14,46 @@ image = cv.imread(parser.parse_args().image)
 height, width, channels = image.shape
 resize_factor = RESIZED_IMAGE_WIDTH/width
 
-image = cv.resize(image, (0, 0), fx=resize_factor, fy=resize_factor, interpolation=cv.INTER_AREA)
+image = cv.resize(image, (0, 0), fx=resize_factor,
+                  fy=resize_factor, interpolation=cv.INTER_AREA)
 
 processed_image = helpers.preprocess_image(image)
-bounds, edged = helpers.find_edges(processed_image)
+bounds, bounded_image = helpers.find_bounds(processed_image)
 
 numbers = helpers.find_numbers(processed_image, bounds)
 
 for number in numbers:
     x, y, w, h = number
-    length = int(w * 2)
-    bound_x = int(x + w // 2 - length // 2)
-    bound_y = int(y + h // 2 - length // 2)
-    bound_w = bound_x + length
-    bound_h = bound_y + length
+    side = int(max(w, h) * 1.2)
+    x1 = int(x + w // 2 - side // 2)
+    y1 = int(y + h // 2 - side // 2)
+    x2 = x1 + side
+    y2 = y1 + side
 
     # find region of interest (roi)
-    roi = processed_image[bound_y:bound_h, bound_x:bound_w]
+    roi = processed_image[y1:y2, x1:x2]
 
     # if the region of interst has a width or height
     if roi.any():
 
         # preprocess the number
-        roi_hog_fd = preprocessor.transform(helpers.preprocess_number(roi, 28, 3))
+        roi_hog_fd = preprocessor.transform(
+            helpers.preprocess_number(roi, 28, 3))
 
         # classify the number
         number = classifier.predict(roi_hog_fd)
 
         # write the text on the original image
-        cv.putText(image, str(int(number[0])), (x, y), cv.FONT_HERSHEY_DUPLEX, 2, (0, 0, 0), 3)
+        cv.putText(image, str(int(number[0])), (x, y),
+                   cv.FONT_HERSHEY_DUPLEX, 2, (0, 0, 0), 3)
 
     # draw the rectangles
     color = (255, 0, 0)
     thickness = 5
-    cv.rectangle(image, (bound_x, bound_y), (bound_w, bound_h), color, thickness)
+    cv.rectangle(image, (x1, y1),
+                 (x2, y2), color, thickness)
 
-output = cv.hconcat((image, edged))
+output = cv.hconcat((image, bounded_image))
 cv.imshow('output', output)
 
 
--- src/helpers.py
+++ src/helpers.py
@@ -2,34 +2,43 @@ import cv2 as cv
 import numpy as np
 from skimage.feature import hog
 
-def find_edges(image):
+
+def find_bounds(image):
+    height, width = image.shape
+    image_area = height * width
 
     # Find all the contours in the image
-    image, contours, hier = cv.findContours(image.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    image, contours, hier = cv.findContours(
+        image.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
     # Convert back to BGR space
-    edged = cv.cvtColor(image, cv.COLOR_GRAY2BGR)
+    bounded = cv.cvtColor(image, cv.COLOR_GRAY2BGR)
 
     # Find the outer most rectangle
     for contour in contours:
         epsilon = 0.02 * cv.arcLength(contour, True)
         approx = cv.approxPolyDP(contour, epsilon, True)
-        if len(approx) == 4 and abs(cv.contourArea(approx)) > 1000 and cv.isContourConvex(approx):
+        expected_bounding_area = 0.5 * image_area
+        if len(approx) == 4 and abs(cv.contourArea(approx)) > expected_bounding_area and cv.isContourConvex(approx):
             color = (0, 0, 255)
             thickness = 3
-            cv.drawContours(edged, [approx], 0, color, thickness)
-            return (approx, edged)
-    return None, edged
+            cv.drawContours(bounded, [approx], 0, color, thickness)
+            return (approx, bounded)
+    return None, bounded
+
 
 def preprocess_image(image):
 
     gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     blurred_image = cv.GaussianBlur(gray_image, (7, 7), 0)
-    threshold_image = cv.adaptiveThreshold(blurred_image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,                                             cv.THRESH_BINARY_INV,3,2)
+    threshold_image = cv.adaptiveThreshold(
+        blurred_image, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 3, 2)
     dilatation_size = 2
-    dilation_element = cv.getStructuringElement(cv.MORPH_RECT, (2*dilatation_size + 1, 2*dilatation_size+1), (dilatation_size, dilatation_size))
+    dilation_element = cv.getStructuringElement(
+        cv.MORPH_RECT, (2*dilatation_size + 1, 2*dilatation_size+1), (dilatation_size, dilatation_size))
     threshold_image = cv.dilate(threshold_image, dilation_element)
     return threshold_image
+
 
 def find_numbers(image, bounds):
 
@@ -41,21 +50,25 @@ def find_numbers(image, bounds):
         image = cv.bitwise_and(black, image)
 
     # find all the contours in the image
-    image, contours, hier = cv.findContours(image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    image, contours, hier = cv.findContours(
+        image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
     numbers = []
     for contour in contours:
         rect = cv.boundingRect(contour)
         x, y, w, h = rect
-        if cv.arcLength(contour, True) > 100:
+        contour_perim_length = 0.1 * min(image.shape[0], image.shape[1])
+        if cv.arcLength(contour, True) > contour_perim_length:
             if bounds is not None and (not cv.pointPolygonTest(bounds, (x, y), False) > 0 or not cv.pointPolygonTest(bounds, (x + w, y + h), False) > 0):
-                    continue
+                continue
             numbers.append(rect)
     return numbers
+
 
 def preprocess_number(roi, length, dilate_width):
     roi = cv.resize(roi, (length, length), interpolation=cv.INTER_AREA)
     roi = cv.dilate(roi, (dilate_width, dilate_width))
-    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), block_norm='L1')        
+    roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(
+        14, 14), cells_per_block=(1, 1), block_norm='L1')
     roi_hog_fd = np.array([roi_hog_fd], 'float64')
     return roi_hog_fd
